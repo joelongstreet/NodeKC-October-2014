298d0041b2d359bebdd650aa5501093d76f4601a
Joe Longstreet <joelongstreet@gmail.com>
2014-10-04 14:09:58 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var app = express();
+var server = http.Server(app);
+var port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', function(req, res){
+  res.send({'ok': true});
+});
+
+server.listen(port, function(){
+  console.log('server started, listening on port ' + port + '...');
+});
