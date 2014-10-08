ef265d88678ef6089537803db8226af4b151e06d
Joe Longstreet <joelongstreet@gmail.com>
2014-10-08 14:36:14 -0500

    seperate into different files for easier comprehension

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ var io = require('socket.io')(server);
 var needle = require('needle');
 var port = process.env.PORT || 3000;
 
+var sup = require('./lib/sup')(io);
+var heyThere = require('./lib/hey-there')(io);
+
+console.log(heyThere);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', function(req, res){
@@ -17,57 +22,6 @@ server.listen(port, function(){
   console.log('server started, listening on port ' + port + '...');
 });
 
-
-
-
-// Hey There demo
-var notifySpark = function(status, next){
-  var url = [
-    'https://api.spark.io/v1/devices/',
-    process.env.SPARK_CORE_ID,
-    '/updateState'
-  ].join('');
-
-  var params = [
-    'access_token=',
-    process.env.SPARK_CORE_TOKEN,
-    '&params=',
-    status
-  ].join('');
-
-  needle.post(url, params, next);
-};
-
-var busy = false;
-var handleStatusUpdate = function(data){
-  if(!busy){
-    busy = true;
-
-    io.sockets.emit('disable-buttons');
-    notifySpark(data.status, function(err, res){
-      busy = false;
-      io.sockets.emit('enable-buttons');
-    });
-  }
-};
-
 io.on('connection', function(socket){
-  socket.on('status-update', handleStatusUpdate);
-});
-
-
-
-
-
-// Phidgets stuff / Sup demo
-var phidgetsLib = require('phidgets');
-var phidget = new phidgetsLib();
-
-phidget.connect(function(){
-  console.log('phidget connected');
-});
-
-phidget.on('input', function(boardId, inputId, state){
-  if(inputId == 1 && state == 1)
-    io.sockets.emit('sup');
+  socket.on('status-update', heyThere.statusUpdate);
 });
